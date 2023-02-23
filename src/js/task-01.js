8f4f703c50c8251c0a02f709be212e34f36ef88a
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-02-23 11:02:54 +0100

    .

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,9 @@
 const categories = document.querySelectorAll('.item');
-// const items = document.querySelectorAll('');
+
 console.log('Number of categories:' + `${categories.length}`);
 
 categories.forEach((category) => {
     console.log('Category:' + category.childNodes[1].textContent);
-    console.log(category.children[1].children.length)
+    console.log(category.children[1].children.length);
 })
 
