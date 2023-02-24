bd633bee01cf03d7d7d442f29f230a26c719f7a4
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-02-24 19:19:11 +0100

    .

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+const inputEmail = document.querySelector('input[type = "email"]');
+const inputPassword = document.querySelector('input[type = "password"]');
 
+const verification = (e) => {
+    e.preventDefault();
+
+    if (inputEmail.value || inputPassword.value === '') {
+        alert('Uzupełnij wszystkie pola'); 
+    } else {
+        let obj = {
+            email: inputEmail.value,
+            password: inputPassword.value
+        };
+       console.log(obj)
+    }
+   
+    
+}
+form.addEventListener('submit',verification)
