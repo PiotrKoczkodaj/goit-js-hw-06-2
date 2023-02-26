c8f721c87c68c4e98d3114791c8daf1912f7ca81
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-02-26 14:21:16 +0100

    .

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -4,10 +4,9 @@ const length = input.getAttribute('data-length');
 const handler = () => {
     
     if (input.value.length > length) {
-        
-        input.classList.add('valid')
+        input.classList.add('valid') || input.classList.replace('invalid','valid')
     } else {
-       input.classList.add('invalid')
+       input.classList.replace('valid','invalid') || input.classList.add('invalid')
     }
     
 }
